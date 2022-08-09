b724082ee4d691a141cf53788d7120abc9aaa81a
Ichtyander <di_ignatov@mail.ru>
2022-08-09 21:29:57 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Degrees.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Degrees
+    {
+        int gradus;
+        int min;
+        int sec;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                gradus = value % 360;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                Gradus += value / 60;
+                min = value % 60;
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                Min += value / 60;
+                sec = value % 60;
+            }
+        }
+
+        public Degrees(int gradus, int min, int sec)
+        {
+            Gradus = gradus;
+            Min = min;
+            Sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            double decimalDegrees = (double)Gradus + (double)Min / 60 + (double)Sec / (60 * 60);
+            return decimalDegrees * Math.PI / 180;
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*1.Угол задан с помощью целочисленных значений gradus - градусов, 
+ * min - угловых минут, sec - угловых секунд. 
+ * Реализовать класс, в котором указанные значения представлены в виде свойств. 
+ * Для свойств предусмотреть проверку 
+ * корректности данных. Класс должен содержать конструктор для установки 
+ * начальных значений, а также метод ToRadians 
+ * для перевода угла в радианы. Создать объект на основе разработанного класса. 
+ * Осуществить использование объекта в программе.*/
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Перевод градусов в радианы");
+            Console.WriteLine();
+            Console.WriteLine("Введите значения угла в градусах");
+
+            Console.Write("\t*градусы - ");
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\t*минуты - ");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\t*секунды - ");
+            int sec = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Degrees degrees = new Degrees(gradus, min, sec);
+
+            Console.WriteLine("Значение угла в радианах - {0}", degrees.ToRadians());
+            Console.ReadKey();
+        }
+    }
+}
